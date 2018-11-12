d74e9129be75c538b5263f9cabb6cf816fa81bce
Tomás Ruiz-López <truizlop@gmail.com>
2018-11-12 10:31:24 +0100

    Initial Commit

---
+++ IntroToBow/IntroToBow.h
@@ -0,0 +1,19 @@
+//
+//  IntroToBow.h
+//  IntroToBow
+//
+//  Created by Tomás Ruiz López on 12/11/2018.
+//  Copyright © 2018 truizlop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntroToBow.
+FOUNDATION_EXPORT double IntroToBowVersionNumber;
+
+//! Project version string for IntroToBow.
+FOUNDATION_EXPORT const unsigned char IntroToBowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntroToBow/PublicHeader.h>
+
+
---
+++ IntroToBowTests/IntroToBowTests.swift
@@ -0,0 +1,34 @@
+//
+//  IntroToBowTests.swift
+//  IntroToBowTests
+//
+//  Created by Tomás Ruiz López on 12/11/2018.
+//  Copyright © 2018 truizlop. All rights reserved.
+//
+
+import XCTest
+@testable import IntroToBow
+
+class IntroToBowTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
